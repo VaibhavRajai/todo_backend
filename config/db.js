ac8907866e1843370fd4aa68b365471d43b646a9
VaibhavRajai <rajaivaibhav28@gmail.com>
2025-06-07 17:03:19 +0530

    Vercel

--- config/db.js
+++ config/db.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
+    // console.log(process.env.MONGO_URI)
     console.log('DB connected');
   } catch (error) {
     console.error('DB connection error:', error);
